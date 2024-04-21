5a1bd207b59cf695bc3314a99b7360d34880d77d
Moh7ven <Mohamedsangare0107@gmail.com>
2024-04-21 14:33:20 +0000

    first commit

---
+++ src/components/IncomeInput/IncomeInput.jsx
@@ -0,0 +1,21 @@
+import s from "./style.module.css";
+
+export function IncomeInput(props) {
+  function updateIncome(e) {
+    const income = e.target.value;
+    console.log("income", income);
+  }
+  return (
+    <div className="row justify-content-center mb-2">
+      <div className={`col-6 ${s.label}`}>Income</div>
+      <div className="col-6">
+        <input
+          onChange={updateIncome}
+          type="number"
+          className="form-control"
+          placeholder="Ex: 3000"
+        />
+      </div>
+    </div>
+  );
+}
