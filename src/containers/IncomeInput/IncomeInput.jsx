1a4627cf0e9c87138732736b93061442ea004dec
Moh7ven <Mohamedsangare0107@gmail.com>
2024-04-21 14:44:27 +0000

    income

--- src/containers/IncomeInput/IncomeInput.jsx
+++ src/containers/IncomeInput/IncomeInput.jsx
@@ -1,15 +1,20 @@
+import { useSelector, useDispatch } from "react-redux";
 import s from "./style.module.css";
+import { setIncome } from "store/expense/expense-slice";
 
 export function IncomeInput(props) {
+  const income = useSelector((store) => store.EXPENSE.income);
+  const dispatch = useDispatch();
   function updateIncome(e) {
     const income = e.target.value;
-    console.log("income", income);
+    dispatch(setIncome(income));
   }
   return (
     <div className="row justify-content-center mb-2">
       <div className={`col-6 ${s.label}`}>Income</div>
       <div className="col-6">
         <input
+          defaultValue={income}
           onChange={updateIncome}
           type="number"
           className="form-control"
